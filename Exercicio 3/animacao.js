48bf33244dcfbba7694a33f395102bfc8d70db49
Gabriela Rocha <gabriela.leme95@hotmail.com>
2022-05-25 09:43:45 -0300

    Ajuste no codigo, removendo classe obsoleta

--- Exercicio 3/animacao.js
+++ Exercicio 3/animacao.js
@@ -1,11 +1,11 @@
-const divlampada = document.querySelector('div.lampada')
-const btnAddRemover = document.querySelector('#on-off')
+const div = document.querySelector('div')
+const btnOnOff = document.querySelector('#on-off')
 
-btnAddRemover.addEventListener('click', () => {
-    if (divlampada.classList.contains('on')) {
-        divlampada.classList.remove('on') // remove classes css do elemento
+btnOnOff.addEventListener('click', () => {
+    if (div.classList.contains('on')) {
+        div.classList.remove('on') // remove classes css do elemento
     } else {
-        divlampada.classList.add('on') // adiciona classes css no elemento
+        div.classList.add('on') // adiciona classes css no elemento
     }
 })
 
